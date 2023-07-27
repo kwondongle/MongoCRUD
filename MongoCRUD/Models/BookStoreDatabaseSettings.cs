cf3bc9948bf3d2aae293a9664798919ba8473601
dh.kwon <dh.kwon@interxlab.com>
2023-07-28 08:55:18 +0900

    초기 커밋.

---
+++ MongoCRUD/Models/BookStoreDatabaseSettings.cs
@@ -0,0 +1,14 @@
+﻿namespace MongoCRUD.Models
+{
+    /// <summary>
+    /// appsettings.json 의 모델링
+    /// </summary>
+    public class BookStoreDatabaseSettings
+    {
+        public string ConnectionString { get; set; } = null!;
+
+        public string DatabaseName { get; set; } = null!;
+
+        public string BooksCollectionName { get; set; } = null!;
+    }
+}
